02752043722aef25a18ba9a9e9e85263a18b7b1e
alejandroHdzV <92843505+alejandroHdzV@users.noreply.github.com>
2023-10-04 20:13:20 -0500

    visualization data

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import MultiStepLR
 from data import ModelNet40
+from vtctestregistrationdata import VTCTestRegistrationData
 import numpy as np
 from torch.utils.data import DataLoader
 from model import PRNet
@@ -37,87 +38,6 @@ import numpy as np
 import os
 from scipy.spatial.transform import Rotation as R
 import h5py
-
-class VTCTestRegistrationData(Dataset):
-
-    def __init__(self, dataset_path, n_points, meter_scaled=True):
-        self.dataset_path = dataset_path
-        self.n_points = n_points
-        self.meter_scaled = meter_scaled
-        
-        with h5py.File(self.dataset_path, "r") as f:
-            self.source = f['source']
-            self.source = self.uniform_downsample_point_cloud(np.asarray(self.source), n_points)
-            self.dataset_size = f['targets'].shape[0]
-            
-        if not self.meter_scaled:
-            self.source = self.source * 1000
-            
-        self.source = torch.from_numpy(self.source).float()
-        
-   
-    def uniform_downsample_point_cloud(self, point_cloud, num_points: int) -> np.ndarray:
-        """
-        Uniformly downsamples a point cloud to a given number of points.
-
-        Args:
-            point_cloud (numpy.ndarray): The original point cloud of shape (N, 3).
-            num_points (int): The desired number of points in the downsampled point cloud.
-
-        Returns:
-            numpy.ndarray: The downsampled point cloud of shape (num_points, 3).
-        """
-        np.random.seed(42)
-        if not isinstance(point_cloud, np.ndarray):
-            point_cloud = np.asarray(point_cloud.points)
-
-        # Ensure num_points is within the range [1, len(point_cloud)]
-        num_points = max(1, min(len(point_cloud), num_points))
-
-        # Shuffle the indices of the original point cloud
-        indices = np.arange(len(point_cloud))
-        np.random.shuffle(indices)
-
-        # Select the first num_points indices and create the downsampled point cloud
-        downsampled_point_cloud = point_cloud[indices[:num_points]]
-
-        return downsampled_point_cloud
-
-    def __len__(self):
-        return self.dataset_size
-    
-    def get_transformations(self, igt):
-        R_ba = igt[:, 0:3, 0:3]								# Ps = R_ba * Pt
-        translation_ba = igt[:, 0:3, 3].unsqueeze(2)		# Ps = Pt + t_ba
-        R_ab = R_ba.permute(0, 2, 1)						# Pt = R_ab * Ps
-        translation_ab = -torch.bmm(R_ab, translation_ba)	# Pt = Ps + t_ab
-        return R_ab[0], translation_ab[0], R_ba[0], translation_ba[0]
-
-    def __getitem__(self, index):
-        
-        with h5py.File(self.dataset_path, "r") as f:
-       
-            template = np.asarray(f['targets'][index])
-            igt = np.asarray(f['transformations'][index])
-        
-        template = self.uniform_downsample_point_cloud(template, self.n_points)
-        template = torch.from_numpy(np.asarray(template)).float()
-        
-        if not self.meter_scaled:
-            template = template * 1000
-            igt[:3,3] = igt[:3,3] * 1000
-
-        igt = torch.from_numpy(igt).float() # igt 4x4, source -> target
-
-        R_ab, translation_ab, R_ba, translation_ba = self.get_transformations(igt.view(1,4,4))
-        euler_ab = torch.from_numpy(R.from_matrix(R_ab.detach().cpu().numpy()).as_euler('zyx')).float()
-        euler_ba = torch.from_numpy(R.from_matrix(R_ba.detach().cpu().numpy()).as_euler('zyx')).float()
-        
-       #  torch.Size([2, 3, 3]) torch.Size([2, 3]) torch.Size([2, 3, 3]) torch.Size([2, 3]) torch.Size([2, 3]) torch.Size([2, 3])
-        
-#         a, b, R_ab, t_at, R_ba, t_ba
-        return template.T, self.source.T, R_ab, translation_ab.view(3), R_ba, translation_ba.view(3), euler_ab.view(3), euler_ba.view(3)
-
 
 
 
---
+++ visualize_data.py
@@ -0,0 +1,74 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from __future__ import print_function
+import torch
+from data import ModelNet40
+import numpy as np
+import torch
+import copy
+import numpy as np
+import open3d as o3d
+from scipy.spatial.transform import Rotation as R
+from vtctestregistrationdata import VTCTestRegistrationData
+
+
+def draw_registration_result(source, target, transformation):
+    SIZE_PERCENTAGE = 0.15
+    v_size = round(max(source.get_max_bound() - source.get_min_bound()) * SIZE_PERCENTAGE, 4)
+
+    source = copy.deepcopy(source)
+    target = copy.deepcopy(target)
+    coord_syst = o3d.geometry.TriangleMesh.create_coordinate_frame(size=v_size, origin=[0, 0, 0])
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0 ])  # 1, 0.706, 0
+    target_temp.paint_uniform_color([0, 0.651, 0.929]) #  # 0, 0.651, 0.929
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp, coord_syst])
+
+def initialize_pointcloud(pts):
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pts)
+    return pcd
+
+
+
+#########
+trainset = ModelNet40(num_points=1024,
+                                             num_subsampled_points=768,
+                                             partition='train', gaussian_noise=True,
+                                             unseen=False, rot_factor=4)
+        
+vtc_path = 'vtc_testing_dataset.hdf5'
+testset = VTCTestRegistrationData(vtc_path, n_points=1024, meter_scaled=True)
+
+#########
+
+
+idx = 3
+test_cts = testset[idx]
+scaling = 2
+t = initialize_pointcloud(test_cts[0].T.detach().cpu().numpy()*scaling)
+s = initialize_pointcloud(test_cts[1].T.detach().cpu().numpy()*scaling)
+transform = np.eye(4)
+transform[:3, :3] = test_cts[2].detach().cpu().numpy()
+transform[:3, 3] = test_cts[3].detach().cpu().numpy() * scaling
+draw_registration_result(t,s,transform)
+
+
+train_cts = trainset[idx]
+t_train = initialize_pointcloud(train_cts[0].T)
+s_train = initialize_pointcloud(train_cts[1].T)
+transform = np.eye(4)
+transform[:3, :3] = train_cts[2]
+transform[:3, 3] = train_cts[3]
+
+print(transform)
+# draw_registration_result(t_train,s_train,transform)
+
+
+
+o3d.visualization.draw_geometries([t_train, s_train, t, s])
